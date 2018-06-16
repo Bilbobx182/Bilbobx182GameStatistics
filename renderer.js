e82d57a4fb4882c5d0feadaea414dcee28dfda92
bilbobx182 <c14474048@mydit.ie>
2018-06-16 12:52:57 +0100

    basic barchart now working

--- renderer.js
+++ renderer.js
@@ -7,27 +7,79 @@ var keyPressData = [
     {key: "Nothing yet!", value: 10}
 ];
 pieChart(keyPressData);
-barChart();
+barChart(keyPressData);
 
 
 //
 
-function barChart() {
-    var data = [30, 86, 168, 281, 303, 365];
-    d3.select(".chart")
-        .selectAll("div")
-        .data(data)
-        .enter()
-        .append("div")
-        .style("width", function (d) {
-            return d + "px";
+function barChart(keyPressdata) {
+
+    var clearBar = document.getElementById('barChart');
+    clearBar.innerHTML = "";
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+// set the ranges
+    var x = d3.scaleBand()
+        .range([0, width])
+        .padding(0.1);
+    var y = d3.scaleLinear()
+        .range([height, 0]);
+
+// append the svg object to the body of the page
+// append a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+    var svg = d3.select("#barChart").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+// get the data
+
+
+    // Scale the range of the data in the domains
+    x.domain(keyPressdata.map(function (d) {
+        return d.key;
+    }));
+    y.domain([0, d3.max(keyPressdata, function (d) {
+        return d.value;
+    })]);
+
+    // append the rectangles for the bar chart
+    svg.selectAll("#barChart")
+        .data(keyPressdata)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function (d) {
+            return x(d.key);
         })
-        .text(function (d) {
-            return d;
+        .attr("width", x.bandwidth())
+        .attr("y", function (d) {
+            return y(d.value);
+        })
+        .attr("height", function (d) {
+            return height - y(d.value);
         });
+
+    // add the x Axis
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    // add the y Axis
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
 }
 
 function pieChart(keyPressData) {
+
+    var test = document.getElementById('chart');
+    test.innerHTML = "";
     var text = "";
 
     var width = 260;
@@ -112,4 +164,5 @@ function pieChart(keyPressData) {
 
 require('electron').ipcRenderer.on('ping', (event, message) => {
     pieChart(message);
+    barChart(message);
 });
